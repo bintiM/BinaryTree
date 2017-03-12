576758663a63e7d52c2e21cc2b4d206a4bf39b8e
bintiM <marc.bintinger@yahoo.com>
2017-03-12 08:00:55 +0100

    initial

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        private Node _root;
+
+        public Node root
+        {
+            get { return _root; }
+            set { _root = value; }
+        }
+        int steps = 0;
+
+        public void add(Node newNode, Node currNode)
+        {
+            if(_root == null)
+            {
+                _root = newNode;
+                return;
+            }
+            if(newNode.data > currNode.data)
+            {
+                if (currNode.right == null)
+                    currNode.right = newNode;
+                else
+                    add(newNode, currNode.right);
+            }
+            else
+            {
+                if (currNode.left == null)
+                    currNode.left = newNode;
+                else
+                    add(newNode, currNode.left);
+            }
+        }
+
+        public Node search(Node currNode, int i)
+        {
+            steps++;
+            if (currNode.data == i)
+            {
+                return currNode;
+            }
+            else
+            {
+                if(i > currNode.data)
+                {
+                    if (currNode.right == null)
+                        return null;
+                    else
+                        return search(currNode.right, i);
+                }
+                else
+                {
+                    if (currNode.left == null)
+                        return null;
+                    else
+                        return search(currNode.left, i);
+                }
+            }
+            //return currNode;
+        }
+        public int searchInt(int i)
+        {
+            Node foundNode = search(_root, i);
+            if (foundNode != null)
+            {
+                Console.WriteLine("gefunden: " + foundNode.data);
+                return steps;
+            }
+            else
+                return 0;
+        }
+
+        public void addInt(int i)
+        {
+            add(new Node(i), _root);
+        }
+        public void preOrder(Node curNode)
+        {
+            Console.Write(curNode.data + " ");
+
+            if (curNode.left != null)
+                preOrder(curNode.left);
+
+            if (curNode.right != null)
+                preOrder(curNode.right);
+        }
+        public void printPreOrder()
+        {
+            preOrder(_root);
+        }
+
+        public void inOrder(Node curNode)
+        {
+            if (curNode.left != null)
+                inOrder(curNode.left);
+
+            Console.Write(curNode.data + " ");
+
+            if (curNode.right != null)
+                inOrder(curNode.right);
+        }
+        public void printInOrder()
+        {
+            inOrder(_root);
+        }
+
+        public void postOrder(Node curNode)
+        {
+            if (curNode.left != null)
+                postOrder(curNode.left);
+            
+            if (curNode.right != null)
+                postOrder(curNode.right);
+
+            Console.Write(curNode.data + " ");
+        }
+        public void printPostOrder()
+        {
+            postOrder(_root);
+        }
+
+        public void min()
+        {
+            Node curNode = _root;
+            while (curNode.left != null)
+                curNode = curNode.left;
+            Console.WriteLine("Min: " + curNode.data);
+        }
+
+        public void max()
+        {
+            Node curNode = _root;
+            while (curNode.right != null)
+                curNode = curNode.right;
+            Console.WriteLine("Max: " + curNode.data);
+        }
+
+
+    }
+
+
+    class Node
+    {
+        private int _data;
+        public int data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+
+        private Node _right;
+
+        public Node right
+        {
+            get { return _right; }
+            set { _right = value; }
+        }
+
+        private Node _left;
+
+        public Node left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+
+        public Node(int i)
+        {
+            _data = i;
+        }
+
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinaryTree bt = new BinaryTree();
+
+            bt.add(new Node(1), null);
+            bt.add(new Node(9), bt.root);
+            bt.add(new Node(5), bt.root);
+            bt.add(new Node(3), bt.root);
+            bt.add(new Node(1), bt.root);
+            bt.add(new Node(7), bt.root);
+
+            bt.printPreOrder();
+            Console.WriteLine();
+            Console.WriteLine("*********************************");
+            bt.printInOrder();
+
+            Console.WriteLine();
+            Console.WriteLine("*********************************");
+            bt.printPostOrder();
+            Console.WriteLine();
+            Console.WriteLine("*********************************");
+            bt.max();
+            Console.WriteLine();
+            Console.WriteLine("*********************************");
+            bt.min();
+
+            #region Vergleiche
+            int max = 5000000;
+
+            Stopwatch sW = new Stopwatch();
+            sW.Start();
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.addInt(0);
+
+            int[] intArray = new int[max];
+            intArray[0] = 0;
+
+            Random r = new Random();
+            
+
+            for (int i = 1; i < max; i++)
+            {
+                // Zufallszahl erzeugen
+                int c = r.Next(0, max);
+
+                //beim Binärbaum hinzufügen
+                binaryTree.addInt(c);
+
+                // im Array einfügen
+                intArray[i] = c;
+
+            }
+
+
+
+            sW.Stop();
+            Console.WriteLine("habe fertig nach :\t\t" + sW.Elapsed);
+
+            //zu suchende Zahl
+            int search = intArray[r.Next(0, max)];
+
+            Console.WriteLine("Suche nach: " + search);
+            //Array Suche
+            sW.Restart();
+            int stepsArray = 0;
+            for (int i = 0; i < max; i++)
+            {
+                if (intArray[i] == search)
+                    break;
+                stepsArray++;
+            }
+            
+            sW.Stop();
+            Console.WriteLine("gefunden:" + intArray[stepsArray]);
+            Console.WriteLine("Array: \t\t" + stepsArray + " Schritte \t" + sW.Elapsed);
+
+            //Binary Suche
+            sW.Restart();
+            int stepsBT = binaryTree.searchInt(search);
+            sW.Stop();
+            Console.WriteLine("Binärbaum: \t" + stepsBT + "Schritte \t" + sW.Elapsed);
+
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
